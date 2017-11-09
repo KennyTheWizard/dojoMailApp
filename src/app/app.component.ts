0f39cafee87590898b20eb6e1fcc2c444b897744
KennyTheWizard <kenngineering@gmail.com>
2017-11-08 19:14:00 -0600

    css problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emails = [
+    {email: 'Bill@gates.com', importance: true, subject: 'New Windows', content: 'Windows XI will launch in year 2100.'},
+    {email: 'Ada@lovelace.com', importance: true, subject: 'Programming', content: 'Enchantress of Numbers'},
+    {email: 'john@carmac.com', importance: false, subject: 'Updated Algo', content: 'New algorithm for shadow volumes.'},
+    {email: 'gabe@newel.com', importance: false, subject: 'HL3!', content: 'Just kidding...'},
+  ]
 }
